38255119a2cc9521bfd4c6d50b1e683bb3e3054e
m4st3 <runtycybin@gmail.com>
2024-09-10 13:37:39 +0200

    initial commit

---
+++ src/main/java/com/cybin/nicodevproject/NicoDevProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cybin.nicodevproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicoDevProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicoDevProjectApplication.class, args);
+	}
+
+}
